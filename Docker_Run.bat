458213f9604486127bdf201affbb65fee340aa9e
Leon <leonbaiyu@gmail.com>
2022-12-24 11:03:53 +0000

    Commodity valuation for pseudo 2D model and experimentation with small-large business model
    Introduction of commodity valuation to pseudo 2D collision model. Other attempts to mess about with small-large business model idea to get company growth correct. Still unsuccessful.
    
    Also, changed directory of docker run bat file

--- Docker_Run.bat
+++ Docker_Run.bat
@@ -1,4 +1,4 @@
 cd ..
 cd C:\Users\LeonWilliams\OneDrive\Python\GitHub_repo\Agent-Based-Simulation-of-Particle-Collisions-and-Traders
-docker run -it --rm -p 8888:8888 --ip 0.0.0.0 -v C:\Users\LeonWilliams\OneDrive:/home/jovyan/OneDrive docker-jupyter-extensible
+docker run -it --rm -p 8888:8888 --ip 0.0.0.0 -v C:\Users\LeonWilliams\OneDrive:/home/jovyan/OneDrive/Python/GitHub_repo/Agent-Based-Simulation-of-Particle-Collisions-and-Traders docker-jupyter-extensible
 cmd /k
